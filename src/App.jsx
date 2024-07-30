37587f10a3361b8a4277e239cb1a85c63b4d117d
venkatsaikrishnareddy <rayapati001@gannon.edu>
2024-07-30 17:24:13 -0400

    react using tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+      <div className="bg-blue-200 w-1/2 h-24 m-5 p-2 border border-blue-500">
+        <h1>Happy Mother's Day Messages, Sayings, Jokes, and Quotes</h1>
+
+        <p>Wish Mom a Happy Mother&aposs Day or say &quotI love you&quot with these heartfelt messages, sayings, jokes, prayers, and quotes that show you care. Moms are so important; they show us what love is, and they help us grow into strong women with families of our own. So</p>
+       </div>
+      );
+
+}
+
+export default App
