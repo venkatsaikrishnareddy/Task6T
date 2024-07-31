3e6ecb80fa63c0701b2e8ee66460435a2fc920cd
venkatsaikrishnareddy <rayapati001@gannon.edu>
2024-07-30 23:48:37 -0400

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 
 import './App.css'
+import Button from './button';
 
 function App() {
   
 
   return (
-      <div className="bg-blue-200 w-1/2 h-24 m-5 p-2 border border-blue-500">
-        <h1>Happy Mother's Day Messages, Sayings, Jokes, and Quotes</h1>
-
-        <p>Wish Mom a Happy Mother&aposs Day or say &quotI love you&quot with these heartfelt messages, sayings, jokes, prayers, and quotes that show you care. Moms are so important; they show us what love is, and they help us grow into strong women with families of our own. So</p>
-       </div>
-      );
-
+    <div className="bg-cover bg-center bg-no-repeat h-screen" style={{ backgroundImage: "url('https://tse1.mm.bing.net/th?id=OIP.3Yi8Q7FETt0r9GvFWIOhIQHaFj&pid=Api&P=0&h=180')" }}>
+    <header className="bg-green-200 text-blue-700 border-4 p-4 rounded-md text-center transition-colors duration-300 hover:bg-green-300">
+      Happy Mother's Day
+    </header>
+    <h1 className="bg-black text-yellow-500 border-4 border-yellow-500 p-4 rounded-md text-center mt-4 transition-transform duration-500 transform hover:scale-105">
+      Happy Mother's Day Messages, Sayings, Jokes, and Quotes
+    </h1>
+    <div className="bg-red-200 w-1/2 h-auto m-5 p-4 border-4 border-red-500 overflow-auto mt-4">
+      <p>Wish Mom a Happy Mother&apos;s Day or say &quot;I love you&quot; with these heartfelt messages, sayings, jokes, prayers, and quotes that show you care. Moms are so important; they show us what love is, and they help us grow into strong women with families of our own.</p>
+    </div>
+  </div>
+);
 }
 
 export default App
---
+++ src/button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Button() {
+  return (
+    <div><button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Click Me</button></div>
+  )
+}
+
+export default Button
